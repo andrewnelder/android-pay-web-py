204afd16f99ed6f2c99ec8484a9f699da5e598f5
Andrew Nelder <anelder@stripe.com>
2017-01-06 17:45:18 -0800

    Fully integrated Android Pay for the Web.  Needs to have the completion method called.
    removed merchant id

--- run.py
+++ run.py
@@ -4,7 +4,7 @@ from flask import Flask, render_template, jsonify, request, Response
 
 STRIPE_SECRET_KEY = os.environ.get("STRIPE_SECRET_KEY")
 STRIPE_PUBLISHABLE_KEY = os.environ.get("STRIPE_PUBLISHABLE_KEY")
-VERIFIED = False
+GOOGLE_MERCHANT_ID = os.environ.get("GOOGLE_MERCHANT_ID")
 
 app = Flask(__name__)
 stripe.api_key = STRIPE_SECRET_KEY
@@ -12,27 +12,34 @@ stripe.api_key = STRIPE_SECRET_KEY
 
 @app.route("/")
 def index():
-    register_domain(request.host.split(':')[0])
     return render_template("index.html", **{
-        "STRIPE_PUBLISHABLE_KEY": STRIPE_PUBLISHABLE_KEY
+        "STRIPE_PUBLISHABLE_KEY": STRIPE_PUBLISHABLE_KEY,
+        "GOOGLE_MERCHANT_ID": GOOGLE_MERCHANT_ID
     })
 
-@app.route("/.well-known/apple-developer-merchantid-domain-association")
-def verify():
-    with open('static/apple-developer-merchantid-domain-association') as fo:
-        return Response(fo.read(), mimetype="text/plain")
-    return ""
 
 @app.route("/charge", methods=["POST"])
 def charge():
     token = request.form.get("token")
     amount = int(float(request.form.get("amount")) * 100)  # convert to int for Stripe
+
+    if token == 'TEST_GATEWAY_TOKEN':
+        print 'Android Pay Test Environment Token Provided.  Mocking it out...'
+        token = stripe.Token.create(
+            card={
+                "number": '4242424242424242',
+                "exp_month": 12,
+                "exp_year": 2018,
+                "cvc": '123'
+            },
+        )
+
     if not token or not amount:
         return "ERROR", 400
 
     # Create Customer
     cus = stripe.Customer.create(
-        description="Apple Pay for the Web Test",
+        description="Android Pay for the Web Test",
         source=token,
         email="test@test.test",
         metadata={
@@ -47,22 +54,6 @@ def charge():
         currency="usd"
     )
     return jsonify({'charge': ch, 'customer': cus})
-
-
-# DOMAIN REGISTRATION #
-
-def register_domain(domain):
-    global VERIFIED
-    if not VERIFIED:
-        print "Attempting to register <{}>...".format(domain)
-        try:
-            stripe.ApplePayDomain.create(domain_name=domain)
-            print "Successful registration!"
-        except Exception as e:
-            pass
-    VERIFIED = True
-
-# END DOMAIN REGISTRATION #
 
 
 if __name__ == "__main__":
